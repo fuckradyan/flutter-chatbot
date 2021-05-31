ae623b68ba211d03a0981d4c6113f372e20c0239
fuckradyan <artem.fagradyan@mail.ru>
2021-06-01 02:55:29 +0300

    da

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, jsonify, request
-import time
+import asyncio
+import websockets  #pip install websockets
 
-app= Flask(__name__)
-@app.route("/bot", method=["POST"])
-
-
-# response
-def response():
-    query = dict(request.form)['query']
-    result = query + " " + time.ctime()
-    return jsonify({"response" : result})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",)
-
+async def echo(websocket, path): # обработка входящего сообщения
+    async for message in websocket: 
+        print("Inbound message:\t", message)
+        if (message=="Текст"):
+            await websocket.send("Пример текстового сообщения")
+        elif (message=="Бот"):
+          await websocket.send("Бот поддерживает команды \"Текст\" и \"Фото\"")
+        elif (message=="Фото"):
+            await websocket.send("Команда \"Фото\" будет реализована в следующей версии!")
+        else:
+            print(message)
+            await websocket.send("Сообщение не распознано. Поддерживаются сообщения формата \"Текст\" и \"Фото\"")
+#TODO самостоятельный ответ сервера
+start_server = websockets.serve(echo, "localhost", 9008)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
